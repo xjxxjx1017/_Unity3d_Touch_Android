c9e8b914d287c921f116ecf972400be8d5298b6c
xjxxjx1017 <xjxxjx1017@gmail.com>
2015-11-17 16:37:57 +1300

    Android implementation

---
+++ Assets/src/TestUnitAction.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestUnitAction : MonoBehaviour {
+
+	// * The unit accesses controler by searching in the object that contains a controller
+	public GameObject ctrlObj;
+	public float moveSpeed = 2.5f;
+
+	// * The controller
+	private ControllerTCLR ctrl;
+	private Transform trans;
+
+	void Start () {
+		trans = GetComponent<Transform>( );
+		ctrl = ctrlObj.GetComponent<ControllerTCLR>( );
+	}
+	
+	void Update() {
+		float dT = Time.deltaTime;
+		float dD = moveSpeed * dT;
+		if( ctrl.isTop )
+			trans.position += new Vector3( 0, dD, 0 );
+		if( ctrl.isCenter )
+			trans.position += new Vector3( 0, dD * -1, 0 );
+		if( ctrl.isLeft )
+			trans.position += new Vector3( dD * -1, 0, 0 );
+		if( ctrl.isRight )
+			trans.position += new Vector3( dD, 0, 0 );
+	}
+}
---
+++ Assets/src/thirdparty/ControllerTCLR.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+// * Use this Controller by attaching it to an empty object in the scene
+public class ControllerTCLR : MonoBehaviour {
+	
+	[HideInInspector]
+	public bool isTop = false;
+	[HideInInspector]
+	public bool isCenter = false;
+	[HideInInspector]
+	public bool isLeft = false;
+	[HideInInspector]
+	public bool isRight = false;
+
+	public void OnTopDown() {
+		isTop = true;
+	}
+	public void OnCenterDown() {
+		isCenter = true;
+	}
+	public void OnLeftDown() {
+		isLeft = true;
+	}
+	public void OnRightDown() {
+		isRight = true;
+	}
+
+	public void OnTopUp() {
+		isTop = false;
+	}
+	public void OnCenterUp() {
+		isCenter = false;
+	}
+	public void OnLeftUp() {
+		isLeft = false;
+	}
+	public void OnRightUp() {
+		isRight = false;
+	}
+}
